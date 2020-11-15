1765f6df66ac003f2941a83fbb170a7e95190335
pswietojanski <p.swietojanski@gmail.com>
2020-11-15 23:41:44 +0000

    swapping download links to zenodo

--- scripts/download_audio.sh
+++ scripts/download_audio.sh
@@ -4,13 +4,13 @@ mkdir -p audio
 
 echo "Downloading slurp audio data..."
 wget -c -q --show-progress  --progress=bar:force \
-     https://pawel-data-public.s3-us-west-2.amazonaws.com/slu/slurp_real.tar.gz \
+     https://zenodo.org/record/4274930/files/slurp_real.tar.gz \
      -O audio/slurp_real.tar.gz 2>&1 | tee audio/slurp_real_download.log 
 
 wget -c -q --show-progress --progress=bar:force \
-     https://pawel-data-public.s3-us-west-2.amazonaws.com/slu/slurp_synth.tar.gz \
+     https://zenodo.org/record/4274930/files/slurp_synth.tar.gz \
      -O audio/slurp_synth.tar.gz 2>&1 | tee audio/slurp_synth_download.log
 
-echo "Extracting packages to audio/slurp*"
-tar -zxvf audio/slurp_real.tar.gz -C audio
-tar -zxvf audio/slurp_synth.tar.gz -C audio
+#echo "Extracting packages to audio/slurp*"
+#tar -zxvf audio/slurp_real.tar.gz -C audio
+#tar -zxvf audio/slurp_synth.tar.gz -C audio
