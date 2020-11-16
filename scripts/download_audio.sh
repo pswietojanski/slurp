c9696d91aed2237adb22d7490674977ef68387d0
pswietojanski <p.swietojanski@gmail.com>
2020-11-16 00:01:16 +0000

    swapping download links to zenodo, update readme

--- scripts/download_audio.sh
+++ scripts/download_audio.sh
@@ -11,6 +11,6 @@ wget -c -q --show-progress --progress=bar:force \
      https://zenodo.org/record/4274930/files/slurp_synth.tar.gz \
      -O audio/slurp_synth.tar.gz 2>&1 | tee audio/slurp_synth_download.log
 
-#echo "Extracting packages to audio/slurp*"
-#tar -zxvf audio/slurp_real.tar.gz -C audio
-#tar -zxvf audio/slurp_synth.tar.gz -C audio
+echo "Extracting packages to audio/slurp*"
+tar -zxvf audio/slurp_real.tar.gz -C audio
+tar -zxvf audio/slurp_synth.tar.gz -C audio
