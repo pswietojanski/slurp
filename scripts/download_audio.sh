2087f816f1061031c0bbbca1b3a6760982d37308
pswietojanski <p.swietojanski@gmail.com>
2021-01-25 23:54:55 +0000

    fix to wget option

--- scripts/download_audio.sh
+++ scripts/download_audio.sh
@@ -2,12 +2,15 @@
 
 mkdir -p audio
 
+wget --help | grep -q '\--show-progress' && \
+  PROGRESS_OPT="--show-progress --progress=bar:force" || PROGRESS_OPT=""
+
 echo "Downloading slurp audio data..."
-wget -c -q --show-progress  --progress=bar:force \
+wget -c -q $PROGRESS_OPT \
      https://zenodo.org/record/4274930/files/slurp_real.tar.gz \
      -O audio/slurp_real.tar.gz 2>&1 | tee audio/slurp_real_download.log 
 
-wget -c -q --show-progress --progress=bar:force \
+wget -c -q $PROGRESS_OPT \
      https://zenodo.org/record/4274930/files/slurp_synth.tar.gz \
      -O audio/slurp_synth.tar.gz 2>&1 | tee audio/slurp_synth_download.log
 
